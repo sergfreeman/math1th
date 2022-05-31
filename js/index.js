03894880201b396cc3aeec857b4e235935a32981
sergfreeman <sergandd@gmail.com>
2022-05-31 22:18:06 +0300

    1

--- js/index.js
+++ js/index.js
@@ -4,16 +4,16 @@ let inputEmailParam = document.querySelector('.teacher-email');
 
 //turn ON/OFF input fields into checkbox
 formCheckboxIsCHECK.addEventListener('click', function () {
-    
+
     if (formCheckboxIsCHECK.checked) {
         inputNameParam.disabled = true;
         inputEmailParam.disabled = true;
-    } 
+    }
     else {
         inputNameParam.disabled = false;
         inputEmailParam.disabled = false;
     }
-    
+
 })
 
 
@@ -22,55 +22,19 @@ let menuOptions = document.querySelectorAll('a');
 menuOptions.forEach(element => {
 
     element.addEventListener('click', function () {
+        var nameOfFile;
         if (formCheckboxIsCHECK.checked) {
-
             inputNameParam.disabled = true;
             let getNameParam = inputNameParam.value;
-
-
-            
             let getEmailParam = inputEmailParam.value;
-            // alert(getNameParam);
-
-
-            let nameOfFile = String(element.className) + '.html?name=' + getNameParam + '&email=' + getEmailParam;
-            window.open(`./${nameOfFile}`, "_blank");
+            nameOfFile = String(element.className) + '.html?name=' + getNameParam + '&email=' + getEmailParam;
+        } else {
+            nameOfFile = String(element.className) + '.html';
         }
-
+        
+        window.open(`./${nameOfFile}`, "_blank");
 
     })
 });
 
-
-// let studentNameField = document.querySelector('.student-name');
-// studentNameField.addEventListener('change', set_student_name);
-
-// function set_student_name() {
-
-
-//     studentName = studentNameField.value;
-//     /two_additions_till10.html?s-name=serg
-
-
-
-// }
-
-// let tmp = document.querySelector('.tmp');
-// tmp.addEventListener('click', function (e) {
-
-//     e.preventDefault();
-
-//     let form = document.querySelector('.authent-form');
-//     form.action = './two_additions_till10.html';
-//     form.method = 'GET';
-
-//     // form.innerHTML = '<input name="q" value="test">';
-
-//     // перед отправкой формы, её нужно вставить в документ
-//     // document.body.append(form);
-
-//     form.submit();
-
-
-// })
 
--- js/two_additions_till10.js
+++ js/two_additions_till10.js
@@ -9,12 +9,13 @@ let firstValue;
 let secondValue;
 let result;
 let attempt = 1;
+let totalReport = [];
 let score = 0;
 
 let attemptCounter = document.querySelector('span');
 let taskField = document.querySelector('.label-tasc-one');
 let answer = document.querySelector('.answer');
-let resultReport = document.querySelector('.resultReport');
+// let resultReport = document.querySelector('.resultReport');
 
 
 
@@ -23,11 +24,14 @@ const urlParams = new URLSearchParams(window.location.search);
 const nameParam = urlParams.get('name');
 
 
-window.addEventListener("load", function() {
-    resultReport.value = `  Учень: ${nameParam} \n`;
-    resultReport.value = resultReport.value + `- початоку уроку (`;
-    resultReport.value += `[${yearIs}:${monthIs}:${dateIs}] [${hoursIs}:${minutesIs}:${secondsIs}]) -`;
-  });
+window.addEventListener("load", function () {
+    // resultReport.value = `  Учень: ${nameParam} \n`;
+    // resultReport.value = resultReport.value + `- початоку уроку ([${yearIs}:${monthIs}:${dateIs}] [${hoursIs}:${minutesIs}:${secondsIs}]) - \n`;
+    totalReport.push(`  Учень: ${nameParam} \n`);
+    totalReport.push(`- початоку уроку ([${yearIs}:${monthIs}:${dateIs}] [${hoursIs}:${minutesIs}:${secondsIs}]) - \n`);
+    // alert(totalReport[1]);
+
+});
 
 //finish
 
@@ -40,7 +44,7 @@ window.addEventListener("load", function() {
 
 document.onkeydown = function (event) {
     if (event.key == 'Enter') {
-        
+
         event.preventDefault();
         checkTask();
         setTask();
@@ -74,19 +78,34 @@ function setTask() {
     taskField.innerHTML = `${firstValue} + ${secondValue} =`;
 
 }
-
+//check on correct answer value, write check report
 function checkTask() {
     if (result == answer.value) {
-        alert('ok');
-
-    } else alert('no');
+        // resultReport.value += `${taskField.innerHTML} ${result} : вірно\n`;
+        totalReport.push(`      ${taskField.innerHTML} ${result} : вірно\n`);
+    } else {
+        // resultReport.value += `${taskField.innerHTML} ${answer.value} : помилка, має бути (${result})\n`;
+        totalReport.push(`      ${taskField.innerHTML} ${answer.value} : помилка, має бути (${result})\n`);
+    };
 
 
     if (attempt > 10) {
-        // document.stopPropagation();
-        window.close();
+        // resultReport.value = resultReport.value + `- кінець уроку ([${yearIs}:${monthIs}:${dateIs}] [${hoursIs}:${minutesIs}:${secondsIs}]) - \n`;
+        totalReport.push(`- кінець уроку ([${yearIs}:${monthIs}:${dateIs}] [${hoursIs}:${minutesIs}:${secondsIs}]) - \n`);
+
+        document.querySelector('.wrapper').remove();
+        let reportArea = document.createElement('textarea');
+        reportArea.className = 'reportAreaClass';
+        reportArea.rows ='14';
+        for(let row of totalReport){
+            reportArea.innerHTML += row;
+        }
+        
+        
+        document.body.appendChild(reportArea);
+
+        // window.close();
         alert('close');
     }
-
 }
 
