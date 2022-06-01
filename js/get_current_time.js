d6c9666634786a066b2fe1ca8b2f06c3ace3c342
sergfreeman <sergandd@gmail.com>
2022-06-01 23:07:26 +0300

    good 01-06-2022

--- js/get_current_time.js
+++ js/get_current_time.js
@@ -1,12 +1,14 @@
-export { yearIs, monthIs, dateIs, hoursIs, minutesIs, secondsIs }
+// export { yearIs, monthIs, dateIs, hoursIs, minutesIs, secondsIs }
 
-let timeNow = new Date();
-let yearIs = timeNow.getFullYear();
-let monthIs = add_zero(timeNow.getMonth() + 1);
-let dateIs = add_zero(timeNow.getDate());
-let hoursIs = add_zero(timeNow.getHours());
-let minutesIs = add_zero(timeNow.getMinutes());
-let secondsIs = add_zero(timeNow.getSeconds());
+// let timeNow = new Date();
+// let yearIs = timeNow.getFullYear();
+// let monthIs = add_zero(timeNow.getMonth() + 1);
+// let dateIs = add_zero(timeNow.getDate());
+// let hoursIs = add_zero(timeNow.getHours());
+// let minutesIs = add_zero(timeNow.getMinutes());
+// let secondsIs = add_zero(timeNow.getSeconds());
+
+export {currentYear, currentMonth, currentDate, currentHour, currentMinute, currentSecond}
 
 function add_zero(value) {
     if (value < 10) {
@@ -14,3 +16,29 @@ function add_zero(value) {
     }
     return value
 }
+
+
+function currentYear(timeNow){
+    return timeNow.getFullYear();
+}
+
+function currentMonth(timeNow){
+    return add_zero(timeNow.getMonth() + 1);
+}
+
+function currentDate(timeNow){
+    return add_zero(timeNow.getDate());
+}
+
+function currentHour(timeNow){
+    return add_zero(timeNow.getHours());
+}
+
+function currentMinute(timeNow){
+    return add_zero(timeNow.getMinutes());
+}
+
+function currentSecond(timeNow){
+    return add_zero(timeNow.getSeconds());
+}
+
--- js/two_additions_till10.js
+++ js/two_additions_till10.js
@@ -1,9 +1,6 @@
-// import  { studentName }  from './index.js';
 import { randomizer } from './random.js';
-import { yearIs, monthIs, dateIs, hoursIs, minutesIs, secondsIs } from './get_current_time.js';
+import { currentYear, currentMonth, currentDate, currentHour, currentMinute, currentSecond } from './get_current_time.js';
 
-
-// alert(test);
 
 let firstValue;
 let secondValue;
@@ -15,36 +12,20 @@ let score = 0;
 let attemptCounter = document.querySelector('span');
 let taskField = document.querySelector('.label-tasc-one');
 let answer = document.querySelector('.answer');
-// let resultReport = document.querySelector('.resultReport');
-
-
 
 //response params (name and email) from index.html
 const urlParams = new URLSearchParams(window.location.search);
 const nameParam = urlParams.get('name');
 
-
+//create Report on load page
 window.addEventListener("load", function () {
-    // resultReport.value = `  Учень: ${nameParam} \n`;
-    // resultReport.value = resultReport.value + `- початоку уроку ([${yearIs}:${monthIs}:${dateIs}] [${hoursIs}:${minutesIs}:${secondsIs}]) - \n`;
     totalReport.push(`  Учень: ${nameParam} \n`);
-    totalReport.push(`- початоку уроку ([${yearIs}:${monthIs}:${dateIs}] [${hoursIs}:${minutesIs}:${secondsIs}]) - \n`);
-    // alert(totalReport[1]);
-
+    totalReport.push
+        (`- початок уроку ([${(currentYear(new Date()))}:${currentMonth(new Date())}:${currentDate(new Date())}] [${currentHour(new Date())}:${currentMinute(new Date())}:${currentSecond(new Date())}]) - \n`);
 });
-
-//finish
-
-
-
-
-
-
-
 
 document.onkeydown = function (event) {
     if (event.key == 'Enter') {
-
         event.preventDefault();
         checkTask();
         setTask();
@@ -64,13 +45,13 @@ button.addEventListener('click', function () {
 
 
 
-
+//create task value like as: ` 1 + 5 = `
 function setTask() {
+    
     attemptCounter.innerHTML = attempt;
     attempt++;
     answer.value = '';
     answer.focus();
-
 
     firstValue = randomizer(0, 10);
     secondValue = randomizer(0, 10 - firstValue);
@@ -81,31 +62,31 @@ function setTask() {
 //check on correct answer value, write check report
 function checkTask() {
     if (result == answer.value) {
-        // resultReport.value += `${taskField.innerHTML} ${result} : вірно\n`;
         totalReport.push(`      ${taskField.innerHTML} ${result} : вірно\n`);
+        score++;
     } else {
-        // resultReport.value += `${taskField.innerHTML} ${answer.value} : помилка, має бути (${result})\n`;
         totalReport.push(`      ${taskField.innerHTML} ${answer.value} : помилка, має бути (${result})\n`);
     };
 
 
     if (attempt > 10) {
-        // resultReport.value = resultReport.value + `- кінець уроку ([${yearIs}:${monthIs}:${dateIs}] [${hoursIs}:${minutesIs}:${secondsIs}]) - \n`;
-        totalReport.push(`- кінець уроку ([${yearIs}:${monthIs}:${dateIs}] [${hoursIs}:${minutesIs}:${secondsIs}]) - \n`);
-
+        totalReport.push(`- кінець уроку ([${(currentYear(new Date()))}:${currentMonth(new Date())}:${currentDate(new Date())}] [${currentHour(new Date())}:${currentMinute(new Date())}:${currentSecond(new Date())}]) - \n`);
+        totalReport.push(`      Оцінка: ${score} балів.`);
         document.querySelector('.wrapper').remove();
         let reportArea = document.createElement('textarea');
         reportArea.className = 'reportAreaClass';
-        reportArea.rows ='14';
-        for(let row of totalReport){
+        reportArea.rows = '14';
+        for (let row of totalReport) {
             reportArea.innerHTML += row;
         }
-        
-        
-        document.body.appendChild(reportArea);
 
-        // window.close();
-        alert('close');
+        document.body.appendChild(reportArea);
+        
+        document.body.addEventListener('click', function(){window.close();})
+        window.onkeydown = function(event){
+            if (event.key == 'Escape') window.close();
+        }
+        
     }
 }
 
